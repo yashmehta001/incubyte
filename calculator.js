448e9bf1da16045eb59ec5bc800bfb27a230123c
yash <yashmehta766@gmil.com>
2024-06-15 15:12:49 +0530

    added logic and test cases for delimiter

--- calculator.js
+++ calculator.js
@@ -1,6 +1,14 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  const nums = numbers.split(/[\n,]/).map(Number);
+
+  let delimiter = /[\n,]/;
+
+  if (numbers.startsWith('//')){
+    const delimiterIndex = numbers.indexOf('\n');
+    delimiter = new RegExp(`[${numbers.charAt(2)}\n,]`);
+    numbers = numbers.substring(delimiterIndex + 1);
+  }
+  const nums = numbers.split(delimiter).map(Number);
   return nums.reduce((sum, num) => sum + num, 0);
 }
 
--- calculator.test.js
+++ calculator.test.js
@@ -19,3 +19,7 @@ test("should return the sum of multiple numbers", () => {
 test("should handle new lines between numbers", () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+test("should support different delimiters", () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
