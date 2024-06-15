6612e1f1d1b92c15c795d5973c44c0885a04fa68
yash <yashmehta766@gmil.com>
2024-06-15 15:05:34 +0530

    added logic and test cases for new line

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  const nums = numbers.split(',').map(Number);
+  const nums = numbers.split(/[\n,]/).map(Number);
   return nums.reduce((sum, num) => sum + num, 0);
 }
 
