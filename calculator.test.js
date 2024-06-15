ab5dced539e9632da32fcdd39b76b84c6ab782eb
yash <yashmehta766@gmil.com>
2024-06-15 14:46:55 +0530

    first passing test

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,5 @@
-const add = require('./calculator');
+const add = require("./calculator");
 
-test('should return 0 for an empty string', () => {
-  expect(add('')).toBe(0);
+test("should return 0 for an empty string", () => {
+  expect(add("")).toBe(0);
 });
